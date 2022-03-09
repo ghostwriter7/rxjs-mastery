fac6095cca4bfe050db63084f8b696c0da351f62
Hubert Rozplochowski <hubert.rozplochowski@gmail.com>
2022-03-09 20:36:58 +0100

    Added bootstrap, tuicss, basic layout

---
+++ src/environments/games.ts
@@ -0,0 +1,7 @@
+export const games: { name: string, description: string, path: string }[] = [
+  { name: 'Alphabet Invasion Game', description: 'Verify your literacy', path: 'alphabet-invasion' },
+  { name: 'Battleship Game', description: 'Be a pirate!', path: 'battleship' },
+  { name: 'Breakout Game', description: 'Run god damnit!', path: 'breakout' },
+  { name: 'Car Racing Game', description: 'Fast and furious', path: 'car-racing' },
+  { name: 'Catch The Dot Game', description: 'Meaw kitty', path: 'catch-the-dot' }
+];
